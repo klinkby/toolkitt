b23414549946564651cef8c7bbc59b21be553491
Klinkby <m@kli.dk>
2023-02-14 13:50:27 +0100

    add EventHandlerExtensions

---
+++ src/Klinkby.Toolkitt/EventHandlerExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Klinkby.Toolkitt;
+
+// hat tip to Jon Skeet
+// https://codeblog.jonskeet.uk/2015/01/30/clean-event-handlers-invocation-with-c-6/
+public static class EventHandlerExtensions
+{
+    public static void Raise(this EventHandler? eh, object sender, EventArgs args) =>
+        Interlocked.CompareExchange(ref eh, null, null)?.Invoke(sender, args);
+
+    public static void Raise<T>(this EventHandler<T>? eh, object sender, T args) where T: EventArgs =>
+        Interlocked.CompareExchange(ref eh, null, null)?.Invoke(sender, args);
+}
---
+++ tests/Klinkby.Toolkitt.Tests/EventHandlerExtensions_Raise_T.cs
@@ -0,0 +1,58 @@
+namespace Klinkby.Toolkitt.Tests;
+
+public class EventHandlerExtensions_Raise_T
+{
+    private static readonly EventArgsMock EventArgsObj = new EventArgsMock();
+
+    private class EventArgsMock : EventArgs
+    { 
+    }
+
+    private class EventMock
+    {
+        public void OnInvoked() => Invoked.Raise(this, EventArgsObj);
+#pragma warning disable S3264 // Events should be invoked
+        public event EventHandler<EventArgsMock>? Invoked;
+#pragma warning restore S3264 // Events should be invoked
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void NoListener_Should_DoNothing()
+    {
+        var mock = new EventMock();
+        mock.OnInvoked();
+       Assert.NotNull(mock); // dummy assertion, just ensure no exceptions raised
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void Subscriber_Should_Raise()
+    {
+        int times = 0;
+        var mock = new EventMock();
+        (object? Sender, EventArgs EventArgs) args = default;
+        mock.Invoked += (sender, ea) =>
+        {
+            args = (sender, ea);
+            times++;
+        };
+        mock.OnInvoked();
+        Assert.Equal(1, times);
+        Assert.Equal(mock, args.Sender);
+        Assert.Equal(EventArgsObj, args.EventArgs);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void Unsubscriber_Should_DoNothing()
+    {
+        int times = 0;
+        var mock = new EventMock();
+        var eh = new EventHandler<EventArgsMock>((object? _, EventArgsMock _) => times++);
+        mock.Invoked += eh;
+        mock.Invoked -= eh;
+        mock.OnInvoked();
+        Assert.Equal(0, times);
+    }
+}
